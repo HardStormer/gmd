6b3f10e2d7a760e4af75bc8356dd64f300146dc8
guzeev.mikhail <guzeev.md@gmail.com>
2023-08-09 15:16:36 +0400

    fix: spinner alert

--- src/shared/ui/spinner/index.tsx
+++ src/shared/ui/spinner/index.tsx
@@ -1,6 +1,6 @@
 const LoadSpinner = () => {
     return(
-        <div className="d-flex align-items-center">
+        <div className="alert alert-info d-flex align-items-center">
             <strong>Загрузка...</strong>
             <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
         </div>
