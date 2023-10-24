7f89a1dd47fec5acb4b5d1bec07d71c7e55d0c64
guzeev.mikhail <guzeev.md@gmail.com>
2023-10-25 03:59:30 +0400

    fix: try to fix server bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './app';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const container = document.getElementById('root') as HTMLElement
-
-const root = ReactDOM.createRoot(
-    container
-);
-
-root.render(
-    <Router>
-        <App />
-    </Router>
-);
-
+// const container = document.getElementById('root') as HTMLElement
+//
+// const root = ReactDOMClient.createRoot(
+//     container
+// );
+//
+// root.render(
+//     <Router>
+//         <App />
+//     </Router>
+// );
+if (typeof window !== 'undefined') {
+    ReactDOM.render(
+        <Router>
+            <App />
+        </Router>,
+        document.getElementById("root"));
+}
